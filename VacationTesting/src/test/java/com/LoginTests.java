6842d7ad743ba615e959a562a2a852077aa5fd81
rarespersa <rarespersa@evz111.evointern>
2014-10-20 15:17:40 +0300

    I created the logintest

---
+++ VacationTesting/src/test/java/com/LoginTests.java
@@ -0,0 +1,42 @@
+package com;
+
+import net.thucydides.core.annotations.Issue;
+import net.thucydides.core.annotations.ManagedPages;
+import net.thucydides.core.annotations.Pending;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Story;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+
+import com.requirements.Application;
+import com.steps.EndUserSteps;
+import com.steps.Loginsteps;
+
+@Story(Application.Search.SearchByKeyword.class)
+@RunWith(ThucydidesRunner.class)
+public class LoginTests {
+
+
+    @Managed(uniqueSession = true)
+    public WebDriver webdriver;
+
+    @ManagedPages(defaultUrl = "http://192.168.1.68:9090/login")
+    public Pages pages;
+
+    @Steps
+    public Loginsteps loginstep;
+
+    @Test
+    public void test(){
+    	
+    	loginstep.login("rares.persa", "1231");
+    }
+    
+    
+} 
