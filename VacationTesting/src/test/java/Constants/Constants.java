be5899ff65ea00b6479d74a52f8bb68760443368
edinamoldvai <edinamoldvai@EVO168.evointern>
2014-10-20 15:24:20 +0300

    DMuser,DMpassword PMuser,PMpassword User,Password //for simple user

--- VacationTesting/src/test/java/Constants/Constants.java
+++ VacationTesting/src/test/java/Constants/Constants.java
@@ -2,5 +2,12 @@ package Constants;
 
 public class Constants {
 	public static final String baseUrl = "http://192.168.1.68:9090/login";
-
+	public static final String DMuser = "rares.persa";
+	public static final String DMpassword = "1231";
+	public static final String PMuser = "edina.moldvai";
+	public static final String PMpassword = "parolanoua";
+	public static final String User = "manuel1";
+	public static final String UserPassword = "manuel1";
+	
+	
 }
