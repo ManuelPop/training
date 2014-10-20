64db08c82b8fdf02dbcab47b2a496cf86a3f8247
manuelpop <manuelpop@EVO417.evointern>
2014-10-20 19:07:19 +0300

    modificare assert

--- VacationTesting/src/test/java/com/steps/MyFreeDaysSteps.java
+++ VacationTesting/src/test/java/com/steps/MyFreeDaysSteps.java
@@ -1,7 +1,6 @@
 package com.steps;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 
 import net.thucydides.core.annotations.Step;
@@ -63,8 +62,7 @@ public class MyFreeDaysSteps extends ScenarioSteps {
 	@Step
     public void verifyMyFreeDaysTitle(String titlu){
         WebElement message = getDriver().findElement(By.cssSelector("div[class='content-title']"));
-        Assert.assertTrue("Test failed! The title was not found!",
-        message.getText().toLowerCase().contains(titlu.toLowerCase()));
+        Assert.assertTrue(message.getText().toLowerCase().contains(titlu.toLowerCase()));
     }
 	
 	
