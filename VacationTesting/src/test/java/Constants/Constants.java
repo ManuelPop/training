9de53f88392ec4a7cd20791d5bdce5458d2ce91a
rarespersa <rarespersa@evz111.evointern>
2014-10-20 15:11:46 +0300

    I created Constants and loginsteps

---
+++ VacationTesting/src/test/java/Constants/Constants.java
@@ -0,0 +1,6 @@
+package Constants;
+
+public class Constants {
+	public static final String baseUrl = "http://192.168.1.68:9090/login";
+
+}
---
+++ VacationTesting/src/test/java/com/steps/Loginsteps.java
@@ -0,0 +1,46 @@
+package com.steps;
+
+import Constants.Constants;
+
+
+import com.pages.DictionaryPage;
+import com.pages.Loginpage;
+
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.annotations.StepGroup;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.core.steps.ScenarioSteps;
+import static ch.lambdaj.Lambda.join;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+
+public class Loginsteps extends ScenarioSteps {
+
+	Loginpage loginpage;
+	
+	@Step
+    	public void insertUsername(String username) {
+			loginpage.enter_username(username);
+	}
+		
+	@Step
+	    public void insertPassword(String password) {
+			loginpage.enter_password(password);
+	}
+		
+	@Step  
+		public void clickSignIn(){
+		loginpage.sign_in();
+		
+    }
+	
+	@StepGroup
+	public void login(String username,String password){
+		
+		getDriver().get(Constants.baseUrl);
+		loginpage.enter_username(username);
+		loginpage.enter_password(password);
+		loginpage.sign_in();
+	}
+}
