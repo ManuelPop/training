f7a8fc7d216105ae0cd4725b349d4fad538c3157
edinamoldvai <edinamoldvai@EVO168.evointern>
2014-10-20 18:29:42 +0300

    added new request test

---
+++ VacationTesting/src/test/java/com/steps/newrequestSteps.java
@@ -0,0 +1,19 @@
+package com.steps;
+
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+
+import com.pages.NewRequestPage;
+
+public class newrequestSteps extends ScenarioSteps {
+
+	NewRequestPage newrequest;
+
+	@Step
+	public void clickOnNewRequest() {
+
+		newrequest.clickOnNewRequest();
+
+	}
+
+}
