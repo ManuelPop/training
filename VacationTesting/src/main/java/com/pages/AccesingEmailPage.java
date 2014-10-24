95174fd5463bf87a16a39cf6ebdce03eed942d32
rarespersa <rarespersa@evz111.evointern>
2014-10-24 11:51:38 +0300

    I created AccesingEmailPage

---
+++ VacationTesting/src/main/java/com/pages/AccesingEmailPage.java
@@ -0,0 +1,37 @@
+package com.pages;
+
+import java.util.Properties;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Store;
+
+import net.thucydides.core.pages.PageObject;
+
+//@DefaultUrl("http://192.168.1.68:9090/home")
+public class AccesingEmailPage extends PageObject {
+
+	public  String readSubject(String emailLink, String emailAddress, String password, String subject) {
+		Properties props = new Properties();
+		props.setProperty("mail.store.protocol", "imaps");
+		try {
+			Session session = Session.getInstance(props, null);
+			Store store = session.getStore();
+			 //props.put("mail.imap.port", "993");
+			props.put("mail.imap.port", "143");
+			store.connect(emailLink, emailAddress, password);
+			 //store.connect(emailLink, emailAddress, password);
+			Folder inbox = store.getFolder("Inbox");
+			inbox.open(Folder.READ_ONLY);
+			Message msg = inbox.getMessage(inbox.getMessageCount());
+			 subject = msg.getSubject();
+			
+			System.out.println("SUBJECT:" + subject);	
+			
+		} catch (Exception mex) {
+			mex.printStackTrace();
+		}
+		return subject;
+	}
+}
