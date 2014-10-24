84029ca6388c33a95b00ce999694607d7c9cff03
rarespersa <rarespersa@evz111.evointern>
2014-10-24 14:50:28 +0300

    I modified AccesingEmailpage

--- VacationTesting/src/main/java/com/pages/AccesingEmailPage.java
+++ VacationTesting/src/main/java/com/pages/AccesingEmailPage.java
@@ -2,8 +2,11 @@ package com.pages;
 
 import java.util.Properties;
 
+import javax.mail.Address;
+import javax.mail.BodyPart;
 import javax.mail.Folder;
 import javax.mail.Message;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Store;
 
@@ -12,51 +15,46 @@ import net.thucydides.core.pages.PageObject;
 //@DefaultUrl("http://192.168.1.68:9090/home")
 public class AccesingEmailPage extends PageObject {
 
-	public  String readSubject(String emailLink, String emailAddress, String password, String emailSubject) {
+	public void readEmail(String emailLink, String emailAddress,String password) {
 		Properties props = new Properties();
 		props.setProperty("mail.store.protocol", "imaps");
 		try {
 			Session session = Session.getInstance(props, null);
 			Store store = session.getStore();
-			 //props.put("mail.imap.port", "993");
+			// props.put("mail.imap.port", "993");
 			props.put("mail.imap.port", "143");
 			store.connect(emailLink, emailAddress, password);
-			 //store.connect(emailLink, emailAddress, password);
+			// store.connect(emailLink, emailAddress, password);
 			Folder inbox = store.getFolder("Inbox");
 			inbox.open(Folder.READ_ONLY);
 			Message msg = inbox.getMessage(inbox.getMessageCount());
-			emailSubject = msg.getSubject();
-			
-			System.out.println("SUBJECT:" + emailSubject);	
-			
+			String emailSubject = msg.getSubject();
+			String sentDate = msg.getSentDate().toString();
+			Object content = msg.getContent();
+			Address[] in = msg.getFrom();
+			for (Address address : in) {
+				System.out.println("SUBJECT:" + emailSubject);
+			}
+			if (content instanceof String) {
+				String body = (String) content;
+				
+				System.out.println("SENT DATE:" + sentDate);
+				System.out.println("SUBJECT:" + emailSubject);
+				System.out.println("CONTENT:" + body);
+			}
+
+			else {
+				Multipart mp = (Multipart) msg.getContent();
+
+				BodyPart bp = mp.getBodyPart(0);
+				System.out.println("SENT DATE:" + sentDate);
+				System.out.println("SUBJECT:" + emailSubject);
+				System.out.println("CONTENT:" + bp.getContent());
+			}
+
 		} catch (Exception mex) {
 			mex.printStackTrace();
 		}
-		return emailSubject;
 	}
-	
-	public  String readBody(String emailLink, String emailAddress, String password, String emailSubject) {
-		Properties props = new Properties();
-		props.setProperty("mail.store.protocol", "imaps");
-		try {
-			Session session = Session.getInstance(props, null);
-			Store store = session.getStore();
-			 //props.put("mail.imap.port", "993");
-			props.put("mail.imap.port", "143");
-			store.connect(emailLink, emailAddress, password);
-			 //store.connect(emailLink, emailAddress, password);
-			Folder inbox = store.getFolder("Inbox");
-			inbox.open(Folder.READ_ONLY);
-			Message msg = inbox.getMessage(inbox.getMessageCount());
-			emailSubject = msg.getSubject();
-			
-			System.out.println("SUBJECT:" + emailSubject);	
-			
-		} catch (Exception mex) {
-			mex.printStackTrace();
-		}
-		return emailSubject;
-	}
-	
-	
+
 }
