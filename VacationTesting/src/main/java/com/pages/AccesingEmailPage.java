4beb4ce8798ee467c25cc478b8160fef27d9fcf4
rarespersa <rarespersa@evz111.evointern>
2014-10-24 13:47:25 +0300

    .

--- VacationTesting/src/main/java/com/pages/AccesingEmailPage.java
+++ VacationTesting/src/main/java/com/pages/AccesingEmailPage.java
@@ -12,7 +12,7 @@ import net.thucydides.core.pages.PageObject;
 //@DefaultUrl("http://192.168.1.68:9090/home")
 public class AccesingEmailPage extends PageObject {
 
-	public  String readSubject(String emailLink, String emailAddress, String password, String subject) {
+	public  String readSubject(String emailLink, String emailAddress, String password, String emailSubject) {
 		Properties props = new Properties();
 		props.setProperty("mail.store.protocol", "imaps");
 		try {
@@ -25,13 +25,38 @@ public class AccesingEmailPage extends PageObject {
 			Folder inbox = store.getFolder("Inbox");
 			inbox.open(Folder.READ_ONLY);
 			Message msg = inbox.getMessage(inbox.getMessageCount());
-			 subject = msg.getSubject();
+			emailSubject = msg.getSubject();
 			
-			System.out.println("SUBJECT:" + subject);	
+			System.out.println("SUBJECT:" + emailSubject);	
 			
 		} catch (Exception mex) {
 			mex.printStackTrace();
 		}
-		return subject;
+		return emailSubject;
 	}
+	
+	public  String readBody(String emailLink, String emailAddress, String password, String emailSubject) {
+		Properties props = new Properties();
+		props.setProperty("mail.store.protocol", "imaps");
+		try {
+			Session session = Session.getInstance(props, null);
+			Store store = session.getStore();
+			 //props.put("mail.imap.port", "993");
+			props.put("mail.imap.port", "143");
+			store.connect(emailLink, emailAddress, password);
+			 //store.connect(emailLink, emailAddress, password);
+			Folder inbox = store.getFolder("Inbox");
+			inbox.open(Folder.READ_ONLY);
+			Message msg = inbox.getMessage(inbox.getMessageCount());
+			emailSubject = msg.getSubject();
+			
+			System.out.println("SUBJECT:" + emailSubject);	
+			
+		} catch (Exception mex) {
+			mex.printStackTrace();
+		}
+		return emailSubject;
+	}
+	
+	
 }
